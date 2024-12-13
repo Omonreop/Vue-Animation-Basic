3f8a7b5c44ed4af1a17f168f63bb67bed5c5b0c8
rafly <rafly.purnomo08@gmail.com>
2024-12-13 12:16:41 +0700

    animation-basic-vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quos
+      iste, quaerat nemo doloribus unde beatae, vero odio, dolor ipsum maxime
+      totam quidem nostrum consequuntur voluptate aliquam doloremque blanditiis
+      temporibus.
+    </p>
+  </div>
+</template>
+
+<style>
+.about {
+  max-width: 300px;
+  margin: 0 auto;
+}
+</style>
